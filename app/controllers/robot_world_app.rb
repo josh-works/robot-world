83b4f687c2f14bf6d6e99a7dafad3e5bebdc52d0
Josh Thompson <thompsonjoshd@gmail.com>
2017-03-15 07:26:40 -0600

    add root and /robots

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -3,4 +3,16 @@ require_relative '../models/robot.rb'
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
+  get '/' do
+    'please go to <pre><a href="/robots">/robots</a></pre> instead '
+  end
+
+  get '/robots' do
+    erb :index
+  end
+
+
+
+
+
 end
